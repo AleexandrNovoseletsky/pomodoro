c2a47f2a4f0e5df98d2f1a71aa2bd7d01fbf5c3e
Alexandr Novoseletsky <night2324@yandex.ru>
2025-10-22 11:51:07 +0400

    Add validation for fields name, pomodoro count.

--- schemas/Category.py
+++ schemas/Category.py
@@ -1,13 +1,27 @@
 from typing import Optional
 
-from fastapi.utils import BaseModel
+from pydantic import BaseModel, Field, field_validator
 
+from settings import Settings
+
+
+settings = Settings()
+validator_params: dict = {
+    'min_length': settings.MIN_CATEGORY_NAME_LENGTH,
+    'max_length': settings.MAX_CATEGORY_NAME_LENGTH,
+    'field_name': 'название категории'
+}
+name_field_params = validator_params.copy()
+name_field_params['description'] = name_field_params.pop('field_name')
 
 class CreateCategorySchema(BaseModel):
-    name: str
+    name: str = Field(..., **name_field_params)
 
 class UpdateCategorySchema(BaseModel):
-    name: Optional[str] = None
+    name: Optional[str] = Field(
+        None,
+        ** name_field_params
+    )
 
 class ResponseCategorySchema(CreateCategorySchema):
     id: int
